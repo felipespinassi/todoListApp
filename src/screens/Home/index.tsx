bc8904e22af27c1ecbd5abbe2b30926c3dc662f6
felipespinassi <felipe_spinassi@hotmail.com>
2023-01-09 13:02:51 -0300

    arrumando estilização

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
       <Text style={{ color: "#fff" }}>todo</Text>
       <View style={styles.container}>
         <TextInput
+          placeholderTextColor={"#808080"}
           style={styles.input}
           placeholder="Adicione uma nova tarefa"
         />
@@ -34,13 +35,16 @@ const styles = StyleSheet.create({
     color: "#808080",
     marginRight: 5,
     borderRadius: 5,
-    height: 40,
+    height: 60,
     width: 300,
+    borderColor: "#5E60CE",
+    fontSize: 20,
   },
   button: {
     backgroundColor: "#1E6F9F",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 5,
+    paddingHorizontal: 25,
+    paddingVertical: 20,
+    borderRadius: 10,
+    color: "#F2F2F2",
   },
 });
