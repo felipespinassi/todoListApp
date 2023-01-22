d5ea909495dc9b572498835925229742bf113634
felipespinassi <felipe_spinassi@hotmail.com>
2023-01-22 01:49:21 -0300

    adicionando statusBar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
+import { StatusBar } from "react-native";
 import Home from "./src/screens/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
+  );
 }
